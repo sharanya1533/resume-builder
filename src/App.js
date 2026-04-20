e4c45c2886e04ccb8aab73f24559f5ccc06a7366
sharanya1533 <sharanyabusetty@gmail.com>
2026-04-20 22:23:09 +0530

    fix case-sensitive import issue

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import React from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import Builder from "./Builder";
+import Builder from "./Builder"; // make sure path is correct
+
+function Home() {
+  return <h2>Home Page</h2>;
+}
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Navigate to="/builder" />} />
+        {/* redirect "/" to "/builder" */}
+        <Route path="/" element={<Navigate to="/builder" replace />} />
+        
+        {/* actual builder page */}
         <Route path="/builder" element={<Builder />} />
+
+        {/* fallback (VERY IMPORTANT) */}
+        <Route path="*" element={<h2>Page Not Found</h2>} />
       </Routes>
     </BrowserRouter>
   );
 }
+
+export default App;
