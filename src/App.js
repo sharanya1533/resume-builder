0658824e2362a335c0e66c5a5f8a479c053fd9c3
sharanya1533 <sharanyabusetty@gmail.com>
2026-04-20 22:29:22 +0530

    remove routing for deployment fix

--- src/App.js
+++ src/App.js
@@ -1,26 +1,8 @@
 import React from "react";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import Builder from "./Builder"; // make sure path is correct
-
-function Home() {
-  return <h2>Home Page</h2>;
-}
+import Builder from "./Builder"; // or "./builder" if lowercase
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        {/* redirect "/" to "/builder" */}
-        <Route path="/" element={<Navigate to="/builder" replace />} />
-        
-        {/* actual builder page */}
-        <Route path="/builder" element={<Builder />} />
-
-        {/* fallback (VERY IMPORTANT) */}
-        <Route path="*" element={<h2>Page Not Found</h2>} />
-      </Routes>
-    </BrowserRouter>
-  );
+  return <Builder />;
 }
 
 export default App;
